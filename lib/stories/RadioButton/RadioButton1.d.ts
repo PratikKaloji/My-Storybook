9ceae6e049df6dcd5ebecddac25163ed7566f56f
PratikKaloji <pratikkaloji99@gmail.com>
2024-02-26 14:22:25 +0530

    c

--- lib/stories/RadioButton/RadioButton1.d.ts
+++ lib/stories/RadioButton/RadioButton1.d.ts
@@ -1,8 +1,8 @@
-/// <reference types="react" />
+import React from "react";
 import "./RadioButton1.css";
 export interface Props {
     label: string;
     disabled: boolean;
     checked: boolean;
 }
-export declare const RadioButton1: (props: Props) => import("react").JSX.Element;
+export declare const RadioButton1: (props: Props) => React.JSX.Element;
